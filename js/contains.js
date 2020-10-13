cc2c8ff07a47eda8c85da4034c67fcc4bb6d836e
hachiko3000 <67519851+hachiko3000@users.noreply.github.com>
2020-10-12 20:39:21 -0500

    Create contains.js

---
+++ js/contains.js
@@ -0,0 +1,5 @@
+// contains_.contains(list, value, [fromIndex]) Aliases: include, includes
+// Returns true if the value is present in the list. Uses indexOf internally, if list is an Array. Use fromIndex to start your search at a given index.
+
+_.contains([1, 2, 3], 3);
+=> true
