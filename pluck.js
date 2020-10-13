2f090436fa6a524f8f580f07a4ff5979003184af
hachiko3000 <67519851+hachiko3000@users.noreply.github.com>
2020-10-12 20:38:05 -0500

    Delete pluck.js

--- pluck.js
+++
@@ -1,6 +0,0 @@
-// pluck_.pluck(list, propertyName)
-// A convenient version of what is perhaps the most common use-case for map: extracting a list of property values.
-
-var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
-_.pluck(stooges, 'name');
-=> ["moe", "larry", "curly"]
