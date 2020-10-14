614ff5540f3dd0b5280fb943bad0f3df93e1ad5e
JuanCarlosLP <jclarios.js@gmail.com>
2020-10-13 20:01:57 -0500

    Contains Function

--- js/contains.js
+++ js/contains.js
@@ -1,5 +1,16 @@
-// contains_.contains(list, value, [fromIndex]) Aliases: include, includes
-// Returns true if the value is present in the list. Uses indexOf internally, if list is an Array. Use fromIndex to start your search at a given index.
+const ages = [5, 8, 10, 34, 2, 56, 34];
 
-_.contains([1, 2, 3], 3);
-=> true
+const myContains = (array, callback) => {
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i])) {
+            return callback(array[i])
+        }
+    }
+}
+
+const get34 = (age) => {
+    return age === 34;
+}
+
+const agesContains34 = myContains(ages, get34);
+console.log(agesContains34);
--- js/jc.js
+++
@@ -1,9 +0,0 @@
-const jc = {};
-
-jc.each = (array, callback) => {
-    for (let i = 0; i < array.length; i++) {
-        callback(array[i], i);
-    }
-}
-
-export default jc;
