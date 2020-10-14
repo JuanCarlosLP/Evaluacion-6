339500c4ce4a83942752fd6ad69ccacec429f663
JuanCarlosLP <jclarios.js@gmail.com>
2020-10-13 20:53:49 -0500

    pluck

--- js/pluck.js
+++ js/pluck.js
@@ -1,6 +1,9 @@
-// pluck_.pluck(list, propertyName)
-// A convenient version of what is perhaps the most common use-case for map: extracting a list of property values.
+var array = {
+    name: ["Pedro", "Juan", "Francisco", "Diego", "Jesica"],
+    ID: [1, 2, 3, 4]
+};
+console.log(pluck(array, 'name'));
 
-var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
-_.pluck(stooges, 'name');
-=> ["moe", "larry", "curly"]
+function pluck(array, key) {
+    return (array[key]);
+};
